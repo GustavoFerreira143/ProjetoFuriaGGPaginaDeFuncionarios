f18ea76d91e2ce005c2bb900d0960935423c6e4a
Gustavo Ferreira de Oliveira Coelho <gustavo.feneita1@gmail.com>
2025-05-02 09:19:12 -0300

    Correcao Erro

--- src/pages/Dashboard/DashboardOptions/AtualizaNoticias/AtualizaNoticias.tsx
+++ src/pages/Dashboard/DashboardOptions/AtualizaNoticias/AtualizaNoticias.tsx
@@ -284,15 +284,15 @@ function AtualizaNoticias() {
                 texto: 'Furiagg anuncia patrocínio de adidasbrasil e nova jersey para Conferir basta ir no site https://www.furia.gg .',
               }
             ].map((noticia, i) => {
-              const textoPlano = typeof noticia.mensagem === 'string' ? noticia.mensagem : '';
+              const textoPlano = typeof noticia.texto === 'string' ? noticia.texto : '';
               const textoComLinks = parseMensagemComLinks(textoPlano);
 
               const textoCortado = textoPlano.length > 100 ? textoPlano.slice(0, 100) + '...' : textoComLinks;
               const excedeLimite = textoPlano.length > 100;
 
               return (
-                <div key={i} className="inline-block lg:w-[50%] h-110 w-[100%] md:w-[80%] md:h-150 bg-black text-white text-center mx-2 rounded-lg shadow-lg">
-                  <img src={noticia.imagem} className='w-full h-[80%] rounded-lg' />
+                <div key={i} className="inline-block lg:w-[50%] h-110 w-[100%] md:w-[80%] md:h-150 bg-white text-white text-center mx-2 rounded-lg shadow-lg">
+                  <img src={noticia.img} className='w-full h-[80%] rounded-lg' />
                   <p className='mt-4 px-4 h-[20%] overflow-hidden text-wrap'>
                     <span dangerouslySetInnerHTML={{ __html: textoCortado }} />
                     {excedeLimite && (
@@ -300,7 +300,7 @@ function AtualizaNoticias() {
                         className="text-blue-400 ml-2 underline cursor-pointer"
                         onClick={() => {
                           setNoticiaSelecionada({
-                            img: noticia.imagem,
+                            img: noticia.img,
                             texto: textoPlano,
                           });
                           setMostrarModal(true);
